0a25469f7a3228f8f61ecb7ed3d6b1c486c56cba
Sander <sander@plex.tv>
2018-07-13 14:41:53 +0200

    Update xpaths, remove old code

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -18,57 +18,48 @@ def MainMenu():
 	oc = ObjectContainer()
 
 	oc.add(DirectoryObject(
-		key = Callback(Videos, title="Laatste journaaluitzendingen", url='http://nos.nl/uitzending/nos-journaal'),
-		title = "Laatste journaaluitzendingen"
+		key = Callback(Videos, title="NOS Journaal", url='https://nos.nl/uitzending/nos-journaal'),
+		title = "NOS Journaal"
 	))
 	oc.add(DirectoryObject(
-		key = Callback(Videos, title="Nieuwsuur", url='http://nos.nl/uitzending/nieuwsuur'),
+		key = Callback(Videos, title="Nieuwsuur", url='https://nos.nl/uitzending/nieuwsuur'),
 		title = "Nieuwsuur"
 	))
 	oc.add(DirectoryObject(
-		key = Callback(Videos, title="NOS op 3", url='http://nos.nl/uitzending/nos-op-3'),
-		title = "NOS op 3"
-	))
-	oc.add(DirectoryObject(
-		key = Callback(Videos, title="NOS Jeugdjournaal", url='http://nos.nl/uitzending/nos-jeugdjournaal'),
+		key = Callback(Videos, title="NOS Jeugdjournaal", url='https://nos.nl/uitzending/nos-jeugdjournaal'),
 		title = "NOS Jeugdjournaal"
 	))
 	oc.add(DirectoryObject(
-		key = Callback(Videos, title="NOS Sportjournaal", url='http://nos.nl/uitzending/nos-sportjournaal'),
-		title = "NOS Sportjournaal"
-	))
-	oc.add(DirectoryObject(
-		key = Callback(Videos, title="NOS Studio Sport", url='http://nos.nl/uitzending/nos-studio-sport'),
+		key = Callback(Videos, title="NOS Studio Sport", url='https://nos.nl/uitzending/nos-studio-sport'),
 		title = "NOS Studio Sport"
 	))
 	oc.add(DirectoryObject(
-		key = Callback(Videos, title="NOS Studio Sport Eredivisie", url='http://nos.nl/uitzending/nos-studio-sport-eredivisie'),
+		key = Callback(Videos, title="NOS Studio Sport Eredivisie", url='https://nos.nl/uitzending/nos-studio-sport-eredivisie'),
 		title = "NOS Studio Sport Eredivisie"
 	))
 	oc.add(DirectoryObject(
-		key = Callback(Videos, title="NOS Studio Voetbal", url='http://nos.nl/uitzending/nos-studio-voetbal'),
+		key = Callback(Videos, title="NOS Studio Voetbal", url='https://nos.nl/uitzending/nos-studio-voetbal'),
 		title = "NOS Studio Voetbal"
 	))
 
 	return oc
 
 ####################################################################################################
-@route('/video/nos/videos', page=int, allow_sync=True)
-def Videos(title, url, page=1):
+@route('/video/nos/videos', allow_sync=True)
+def Videos(title, url):
 
 	oc = ObjectContainer(title2=title)
-	page_url = url % (page) if '%d' in url else url
-	content = HTML.ElementFromURL(page_url)
-	rbar = content.xpath('.//div[contains(@class, "broadcast-player__playlist")]')[0]
+	html = HTML.ElementFromURL(url)
+	sidebar = html.xpath('.//div[@class="broadcast-player__playlist__list"]')[0]
 
-	for video in rbar.xpath('.//a[contains(@class, "broadcast-link")]'):
+	for video in sidebar.xpath('.//li[@class="broadcast-player__playlist__item"]'):
 
-		video_url = video.xpath('./@href')[0]
+		video_url = video.xpath('./a/@href')[0]
 
-		if not video_url.startswith('http://'):
-			video_url = 'http://nos.nl/%s' % video_url.lstrip('/')
+		if not video_url.startswith('https://'):
+			video_url = 'https://nos.nl/%s' % video_url.lstrip('/')
 
-		video_title = video.xpath('./span[contains(@class, "broadcast-link__name")]/text()')[0].strip() + ' ' + video.xpath('./time[contains(@class, "broadcast-link__date")]/text()')[0].strip()
+		video_title = video.xpath('.//time/text()')[0].strip()
 
 		oc.add(VideoClipObject(
 			url = video_url,
@@ -78,12 +69,5 @@ def Videos(title, url, page=1):
 
 	if len(oc) < 1:
 		return ObjectContainer(header="Geen video's", message="Deze directory bevat geen video's")
-
-	if len(content.xpath('//div[@id="paging"]//a[contains(text(), "Volgende")]')) > 0:
-
-		oc.add(NextPageObject(
-			key = Callback(Videos, title=title, url=url, page=page+1),
-			title = 'Meer ...'
-		))
 
 	return oc
